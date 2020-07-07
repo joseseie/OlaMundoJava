e63a278f99675f4157161efb879886cc4d42b78b
Jose Seie <joseeugenioseie@hotmail.com>
2020-07-07 23:29:35 +0200

    Adição de olá mundo 2.

--- src/mz/co/explicador/OlaMundo.java
+++ src/mz/co/explicador/OlaMundo.java
@@ -1,15 +1,28 @@
 
 package mz.co.explicador;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author joseseie
  */
 public class OlaMundo {
     
+    // Exercício: Mostrar olá mundo de 3 formas diferentes.
+    
     public static void main(String[] args) {
         
         System.out.println("Olá Mundo!!");
+        
+        olaMundo2();
+        
+    }
+    
+    
+    public static void olaMundo2 () {
+        
+        JOptionPane.showMessageDialog(null, "Olá Mundo");
         
     }
     
